568af31dc3e8ab108c05da21227442a7fe9bce11
Agustin Villalba <villalbaagustin5397@gmail.com>
2024-05-16 10:18:08 -0300

    Arreglo encuentra html

--- CalculadoraSueldos.py
+++ CalculadoraSueldos.py
@@ -1,9 +1,25 @@
-import pdfkit 
+import pdfkit
 import jinja2
 from datetime import datetime;
 
-try:
-    print(int(input("Ingrese nombre: ")))
-except ValueError:
-    print("Ingrese nombre correctamente")    
+today_date = datetime.today().strftime('%d %b,%Y')
+nombre = (input("Ingrese nombre: "))
+apellido = (input("Ingrese nombre: "))
+contenido = {'nombre':nombre}
 
+#Lectura del html con ninja y erea
+template_loader = jinja2.FileSystemLoader('./');
+template_env=jinja2.Environment(loader=template_loader);
+
+html_template = 'index.html';
+
+template = template_env.get_template('index.html');
+
+output_Text = template.render(contenido);
+
+config = pdfkit.configuration(wkhtmltopdf= '/usr/bin/wkhtmltopdf');
+
+output_pdf = nombre + apellido + ".pdf";
+
+print(output_pdf)
+pdfkit.from_string(output_Text,output_pdf,configuration=config);
